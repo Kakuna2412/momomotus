b0582ddba9e2a9b94709772628f74fed73980429
Kakuna2412 <35299750+Kakuna2412@users.noreply.github.com>
2018-09-23 02:22:35 +0200

    Disp letters

--- scripts/js/motus.js
+++ scripts/js/motus.js
@@ -1,29 +1,47 @@
-var previewnb;
-var previewnbdisp;
-var preview;
-var previewimg;
-var bigPreview;
-var deck1nb;
-var deck1nbdiff;
-var deck1nbdisp;
-var deck1;
-var deck1img;
-var deck2;
-var myCollectionRemain;
+var cptword;
+var cptlett;
+var words;
+var wordsdisp;
+var solution;
 
 var motusState = {
+	
+	init: function () {
+		words = 0;
+	},	
 	
 	create: function () {
 		
 		//Disp Background
 		game.add.image(0, 0, 'sky');
 		
-		//Button Fight
+		//Button Give up
 		game.add.button(game.world.width-193-8, 8, 'button', this.giveup, this, 2, 1, 0);
 		
 		//Button Fullscreen
 		var button = game.add.button(game.world.width-32-8, game.world.height-32-8, 'buttonfull', gofull, this, 1, 0);
 		button.scale.setTo(0.5,0.5);
+		
+		init();
+		
+		//Disp Words
+		wordsdisp = new Array(6);
+		for (var i=0;i<6;i++){
+			wordsdisp[i] = new Array(8);
+			for (var j=0;j<8;j++){
+				(wordsdisp[i])[j] = game.add.text(220+30*j, 600+30*i, '', {font: "20px Arial", fill: "#ffffff"});
+				(wordsdisp[i])[j].text = "/";
+			}
+		}
+
+	},
+	
+	dispMots: function () {
+		
+	},
+	
+	dispSolution: function () {
+		
 	},
 	
 	update: function () {
@@ -31,7 +49,8 @@ var motusState = {
 	},
 	
 	giveup: function () {
-
+		dispSolution();
+		init();
 	}
 
 
